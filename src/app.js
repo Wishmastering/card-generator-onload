4501430b849c762a72f92f99d6069a4aa4958c2e
Wishmastering <charsdb@gmail.com>
2022-08-19 00:17:16 +0000

    Final - No Color Change

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const suit = ["♦", "♥", "♠", "♣"];
+
+const number = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+window.onload = () => {
+  let rSuit = Math.floor(Math.random() * suit.length);
+  let rNumber = Math.floor(Math.random() * number.length);
+
+  let suit1 = document.querySelector("#addSuit1");
+  suit1.innerHTML = suit[rSuit];
+
+  let suit2 = document.querySelector("#addSuit2");
+  suit2.innerHTML = suit[rSuit];
+
+  let number1 = document.querySelector("#addNumber");
+  number1.innerHTML = number[rNumber];
+
+  function color() {
+    return this.rSuit === "♠" || this.rSuit === "♣" ? "black" : "red";
+  }
 };
+
+onload();
